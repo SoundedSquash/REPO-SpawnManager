4de4dc6b44bd4bbe4222e1c8de6c0c76b9e538e4
SoundedSquash <20405599+SoundedSquash@users.noreply.github.com>
2025-03-26 02:37:57 -0500

    Add level manager.

---
+++ Managers/LevelManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace SpawnManager.Managers
+{
+    public static class LevelManager
+    {
+        private static List<Level> _removedList = new List<Level>();
+
+        public static void RemoveLevels()
+        {
+            if (!RunManagerLevelVariableIsAvailable) return;
+            
+            List<string> disabledLevelNames = Settings.GetDisabledSettingsEntryListNames(Settings.DisabledLevels);
+
+            RunManager.instance.levels.Where(l => disabledLevelNames.Contains(l.name)).ToList().ForEach(level =>
+            {
+                Settings.Logger.LogDebug($"Removed level {level.name}.");
+                _removedList.Add(level);
+                RunManager.instance.levels.Remove(level);
+            });
+            
+            if (RunManager.instance.levels.Count == 0)
+            {
+                Settings.Logger.LogError("No levels left in RunManager. Quitting to Main Menu.");
+                 RunManager.instance.StartCoroutine(RunManager.instance.LeaveToMainMenu());
+            }
+        }
+
+        public static void RestoreLevels()
+        {
+            if (!RunManagerLevelVariableIsAvailable) return;
+            
+            _removedList.ForEach(level =>
+            {
+                Settings.Logger.LogDebug($"Restored level {level.name}.");
+                RunManager.instance.levels.Add(level);
+                _removedList.Remove(level);
+            });
+        }
+
+        private static bool RunManagerLevelVariableIsAvailable => RunManager.instance != null && RunManager.instance.levels != null;
+    }
+}
--- Patches/RunManager.cs
+++ Patches/RunManager.cs
@@ -13,12 +13,25 @@ namespace SpawnManager.Patches
         {
             if (SemiFunc.MenuLevel())
             {
+                LevelManager.RestoreLevels();
                 ValuableManager.RestoreValuableObjects();
                 return;
             }
 
             Settings.Logger.LogDebug("Removing valuables.");
             ValuableManager.RemoveValuables();
+        }
+        [HarmonyPatch(nameof(RunManager.SetRunLevel))]
+        [HarmonyPrefix]
+        static void RunManagerChangeLevelPrefix(RunManager __instance, ref Level ___previousRunLevel)
+        {
+            LevelManager.RemoveLevels();
+
+            if (__instance.levels.Count == 1)
+            {
+                // Clear the previous level so it can pick the same one.
+                ___previousRunLevel = null;
+            }
         }
     }
 }
